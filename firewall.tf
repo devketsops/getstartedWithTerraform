bb1688aaacaf4f336d5be95a64b823899de54e41
devketsops <ketan.kanade10@gmail.com>
2026-01-13 11:11:07 +0530

    Update firewall.tf

--- firewall.tf
+++ firewall.tf
@@ -15,21 +15,21 @@ resource "digitalocean_firewall" "control_plane" {
   inbound_rule {
     protocol         = "tcp"
     port_range       = "2379-2380"
-    source_addresses = ["10.122.0.0/20"]
+    source_addresses = ["172.16.0.0/24"]
   }
 
   # Kubernetes API server: Internal VPC communication
   inbound_rule {
     protocol         = "tcp"
     port_range       = "6443"
-    source_addresses = ["10.122.0.0/20"]
+    source_addresses = ["172.16.0.0/24"]
   }
 
   # Kubelet API, Kube-scheduler, Kube-controller-manager: Internal VPC communication
   inbound_rule {
     protocol         = "tcp"
     port_range       = "10248-10260"
-    source_addresses = ["10.122.0.0/20"]
+    source_addresses = ["172.16.0.0/24"]
   }
 
   # Standard Outbound Rules (Allow all IPv4 and IPv6)
@@ -66,21 +66,21 @@ resource "digitalocean_firewall" "node" {
   inbound_rule {
     protocol         = "tcp"
     port_range       = "10250"
-    source_addresses = ["10.122.0.0/20"]
+    source_addresses = ["172.16.0.0/24"]
   }
 
   # Kube-proxy: Internal VPC communication
   inbound_rule {
     protocol         = "tcp"
     port_range       = "10256"
-    source_addresses = ["10.122.0.0/20"]
+    source_addresses = ["172.16.0.0/24"]
   }
 
   # NodePort Services: Internal VPC communication
   inbound_rule {
     protocol         = "tcp"
     port_range       = "30000-32767"
-    source_addresses = ["10.122.0.0/20"]
+    source_addresses = ["172.16.0.0/24"]
   }
 
   # Standard Outbound Rules (Allow all IPv4 and IPv6)
